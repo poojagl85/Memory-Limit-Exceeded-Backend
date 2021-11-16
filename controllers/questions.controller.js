7124c7c9a86e28103c4fcb73e11e6d08d660d538
Pooja Goyal <poojagl85@gmail.com>
2021-11-16 11:49:09 +0530

    Moderation feature and question post update

--- controllers/questions.controller.js
+++ controllers/questions.controller.js
@@ -2,51 +2,94 @@ const Question = require("../models/questions.model");
 const Category = require("../models/category.model");
 const User = require("../models/user.model");
 const slugify = require("slugify");
+const { promisify } = require("util");
+require("@tensorflow/tfjs");
+const toxicity = require("@tensorflow-models/toxicity");
+
+const threshold = 0.9;
+
+const getMaliciousPredictions = (predictions, onSuccess, onError) => {
+	for (let prediction of predictions) {
+		if (prediction.results[0].match === true) {
+			onError({
+				msg: `Your solution/question could not be posted as it consisted of content falling in ${prediction.label} category.`,
+			});
+			return;
+		}
+	}
+	onSuccess({ msg: `Passed...!` });
+};
+
+getMaliciousPredictions[promisify.custom] = (predictions) => {
+	return new Promise((resolve, reject) => {
+		getMaliciousPredictions(
+			predictions,
+			(onSuccess) => resolve(onSuccess),
+			(onError) => reject(onError)
+		);
+	});
+};
 
 exports.postQuestion = async (req, res) => {
 	try {
-		const { title, description, categoryId } = req.body;
-		const slug = slugify(title);
+		await toxicity.load(threshold).then((model) => {
+			const sentences = [req.body.description];
 
-		const question = new Question({
-			title,
-			slug,
-			description,
-			categoryId,
-			authorID: req.user._id,
-		});
+			model.classify(sentences).then((predictions) => {
+				const maliciousPredictions = promisify(getMaliciousPredictions);
+				maliciousPredictions(predictions)
+					.then(async (result) => {
+						const { title, description, categoryId } = req.body;
+						const slug = slugify(title);
 
-		console.log(categoryId);
+						const question = new Question({
+							title,
+							slug,
+							description,
+							categoryId,
+							authorID: req.user._id,
+						});
 
-		const _question = await question.save();
-		const userId = _question.authorID;
+						console.log(categoryId);
 
-		const _cat = await Category.updateOne(
-			{ _id: categoryId },
-			{
-				$push: {
-					questionId: _question._id,
-				},
-			}
-		);
+						const _question = await question.save();
+						const userId = _question.authorID;
 
-		console.log(_cat);
+						const _cat = await Category.updateOne(
+							{ _id: categoryId },
+							{
+								$push: {
+									questionId: _question._id,
+								},
+							}
+						);
 
-		const _user = await User.updateOne(
-			{ _id: userId },
-			{
-				$push: {
-					activityId: _question._id,
-				},
-			}
-		);
+						console.log(_cat);
 
-		return res.status(200).json({
-			message: `Question posted...!`,
-			question: {
-				_question,
-			},
-		});
+						const _user = await User.updateOne(
+							{ _id: userId },
+							{
+								$push: {
+									activityId: _question._id,
+								},
+							}
+						);
+
+						return res.status(200).json({
+							message: `Question posted...!`,
+							question: {
+								_question,
+							},
+						});
+					}).catch((err) => {
+						return res.status(403).json({
+							message: err.msg,
+						});
+					});
+
+			})
+		})
+
 	} catch (error) {
 		console.log(error);
 		return res.status(500).json({
@@ -60,12 +103,13 @@ exports.getQuestions = async (req, res) => {
 		const pageNumber = req.query.page;
 
 		const skip = (pageNumber - 1) * 10;
+		const user = await User.findById(req.query.id);
 
-		const questions = await Question.find({})
+
+		const questions = await Question.find({ categoryId: user.categoryId })
 			.skip(skip)
 			.limit(10)
 			.populate("authorID");
-		console.log(questions);
 		return res.status(200).json({
 			questions,
 		});
@@ -89,7 +133,7 @@ exports.getQuestionDetail = async (req, res) => {
 					select: "fullName email username",
 				},
 			})
-			.populate("categoryId", );
+			.populate("categoryId",);
 
 		return res.status(200).json({
 			question,
--- controllers/solution.controller.js
+++ controllers/solution.controller.js
@@ -7,102 +7,79 @@ const toxicity = require("@tensorflow-models/toxicity");
 
 const threshold = 0.9;
 
-const getMaliciousPredictions = (predictions, callback) => {
-  console.log(predictions);
+const getMaliciousPredictions = (predictions, onSuccess, onError) => {
   for (let prediction of predictions) {
     console.log(prediction.label + " " + prediction.results[0].match);
     if (prediction.results[0].match === true) {
-      return callback(
-        new Error(
-          `Your solution/question could not be posted as it consisted of content falling in ${prediction.label} category.`
-        ),
-        null
-      );
+      onError({
+        msg: `Your solution/question could not be posted as it consisted of content falling in ${prediction.label} category.`,
+      });
+      return;
     }
   }
-
-  return callback(null, "Success");
+  onSuccess({ msg: `Solution added... !` });
 };
 
-// const getSumAsync = (num1, num2, callback) => {
-
-// 	if (!num1 || !num2) {
-// 	  return callback(new Error("Missing arguments"), null);
-// 	}
-// 	return callback(null, num1 + num2);
-//   }
-//   getSumAsync(1, 1, (err, result) => {
-// 	if (err){
-// 	  doSomethingWithError(err)
-// 	}else {
-// 	  console.log(result) // 2
-// 	}
-//   })
-
-//   const getSumPromise = promisify(getSumAsync) // step 1
-// getSumPromise(1, 1) // step 2
-// .then(result => {
-//   console.log(result)
-// })
-// .catch(err =>{
-//   doSomethingWithError(err);
-// })
+getMaliciousPredictions[promisify.custom] = (predictions) => {
+  return new Promise((resolve, reject) => {
+    getMaliciousPredictions(
+      predictions,
+      (onSuccess) => resolve(onSuccess),
+      (onError) => reject(onError)
+    );
+  });
+};
 
 exports.addSolution = async (req, res) => {
   try {
-    console.log(req.body);
-    console.log(req.user._id);
-
     await toxicity.load(threshold).then((model) => {
       const sentences = [req.body.description];
 
       model.classify(sentences).then((predictions) => {
-        const promise = promisify(getMaliciousPredictions);
-        promise(predictions, (err, result) => {
-          if (err) {
-            console.log("In promise callback error block");
-            return res.status(403).json({
-              message: err.message,
+        const maliciousPredictions = promisify(getMaliciousPredictions);
+        maliciousPredictions(predictions)
+          .then(async (result) => {
+            const { description, questionId } = req.body;
+            const authorID = req.user._id;
+
+            const solution = new Solution({
+            	description,
+            	questionId,
+            	authorID,
             });
-          } else {
-            console.log("In promise callback result  block");
+
+            const _solution = await solution.save();
+
+            await Question.updateOne(
+            	{ _id: questionId },
+            	{
+            		$push: {
+            			solutionId: _solution._id,
+            		},
+            	}
+            );
+
+            await User.updateOne(
+            	{ _id: authorID },
+            	{
+            		$push: {
+            			activityId: _solution._id,
+            		},
+            	}
+            );
+
+            console.log(_solution);
             return res.status(200).json({
-              message: `Solution added... !`,
+              message: result.msg,
             });
-          }
-
-          // const { description, questionId } = req.body;
-          // const authorID = req.user._id;
-
-          // const solution = new Solution({
-          // 	description,
-          // 	questionId,
-          // 	authorID,
-        });
+          })
+          .catch((err) => {
+            return res.status(403).json({
+              message: err.msg,
+            });
+          });
       });
     });
-
-    // const _solution = await solution.save();
-
-    // const _question = await Question.updateOne(
-    // 	{ _id: questionId },
-    // 	{
-    // 		$push: {
-    // 			solutionId: _solution._id,
-    // 		},
-    // 	}
-    // );
-
-    // const _user = await User.updateOne(
-    // 	{ _id: authorID },
-    // 	{
-    // 		$push: {
-    // 			activityId: _solution._id,
-    // 		},
-    // 	}
-    // );
-
-    // console.log(_solution);
   } catch (error) {
     console.log(error);
     return res.status(500).json({
