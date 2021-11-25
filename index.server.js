fe3d02a4fbebfad6ef459e83e5be30f776697196
Pooja Goyal <poojagl85@gmail.com>
2021-11-25 12:27:21 +0530

    header change

--- index.server.js
+++ index.server.js
@@ -33,7 +33,7 @@ mongoose
 	});
 
 var corsOptions = {
-	origin: 'https://out-of-memory.vercel.app/',
+	origin: 'https://out-of-memory.vercel.app',
 	optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 	allowedHeaders: '*',
 	exposeHeaders: ['set-cookie']
