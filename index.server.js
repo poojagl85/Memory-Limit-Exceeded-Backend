c65d9216c3c1e0e0d87dae49005462bfcc6c0e9c
Pooja Goyal <poojagl85@gmail.com>
2021-11-25 12:06:33 +0530

    cookie

--- index.server.js
+++ index.server.js
@@ -49,6 +49,7 @@ app.use("/api", solutionRoutes);
 app.use("/api", commentRoutes);
 app.use("/api", fakeRoutes);
 
+
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
