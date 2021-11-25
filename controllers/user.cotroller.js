062e0350684d427b3d2eb9c3bd098369e0228f41
Pooja Goyal <poojagl85@gmail.com>
2021-11-25 13:04:05 +0530

    cookie parameters update

--- controllers/user.cotroller.js
+++ controllers/user.cotroller.js
@@ -56,7 +56,7 @@ exports.signin = async (req, res) => {
 
 
 
-				res.cookie("token", token, { httpOnly: true, secure: true, domain: "out-of-memory.vercel.app", SameSite="None" });
+				res.cookie("token", token, { httpOnly: true, secure: true, domain: "out-of-memory.vercel.app", sameSite: "none" });
 
 				return res.status(200).json({
 					message: "Signin successful..!",
